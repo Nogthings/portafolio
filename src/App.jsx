23a23f4a2a8b8847b0e6a7bcdbcb9cb2d080f70d
Oscar Galvez <gcoscar99@yahoo.com>
2023-05-21 21:54:31 -0700

    Ahora el link para ver el cv se abre en otra pestaña

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,8 @@ export default function App() {
                     <a 
                     href="https://github.com/Nogthings/cv/raw/main/Oscar%20Galvez%20CV.pdf"
                     download="oscargalvez-cv.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800 px-5">
                       Resumen
                     </a>
